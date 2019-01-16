cb6040d53b845ac3abbab54505e711077b42cc22
Mark Ryan <mark.ryan@hmcts.net>
2019-01-16 12:27:27 +0000

    fix json paths

--- scripts/cmc/add-toggles.sh
+++ scripts/cmc/add-toggles.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+export JSON_DIR="/scripts/cmc/"
 if [ ! -z "$VERBOSE" ] && [ -e "$VERBOSE" ]; then
   export CURL_OPTS="-v"
 else
@@ -13,12 +14,12 @@ function add_weighted_toggle() {
 
   echo -e "${FEATURE_TOGGLE_API_URL} ::: adding ${WEIGHTED_TOGGLE_NAME} with weight ${WEIGHT} and ${DESCRIPTION}"
 
-  envsubst < /weighted_toggle.template.json > /"${WEIGHTED_TOGGLE_NAME}".json
+  envsubst < "${JSON_DIR}"weighted_toggle.template.json > "${JSON_DIR}""${WEIGHTED_TOGGLE_NAME}".json
 
   curl ${CURL_OPTS} -X PUT \
    -H 'Content-Type: application/json' \
    -u admin@example.com:Password12 \
-   -d @/"${WEIGHTED_TOGGLE_NAME}".json \
+   -d @"${JSON_DIR}""${WEIGHTED_TOGGLE_NAME}".json \
    "${FEATURE_TOGGLE_API_URL}""${WEIGHTED_TOGGLE_NAME}"
 }
 
@@ -29,14 +30,16 @@ function add_toggle() {
 
   echo -e "Setting ${TOGGLE_NAME} to ${TOGGLE_ENABLED}"
 
-  envsubst < /toggle.template.json > /"${TOGGLE_NAME}".json
+  envsubst < "${JSON_DIR}"toggle.template.json > "${JSON_DIR}""${TOGGLE_NAME}".json
 
   curl ${CURL_OPTS} -X PUT \
    -H 'Content-Type: application/json' \
    -u admin@example.com:Password12 \
-   -d @/"${TOGGLE_NAME}".json \
+   -d @"${JSON_DIR}""${TOGGLE_NAME}".json \
    "${FEATURE_TOGGLE_API_URL}""${TOGGLE_NAME}"
 }
+
+echo -e "VERBOSE=${VERBOSE} ::: JSON_DIR=${JSON_DIR}"
 
 add_weighted_toggle cmc_admissions 'CMC admissions' '1.0'
 add_toggle cmc_defence_reminders 'CMC defence reminders' false
