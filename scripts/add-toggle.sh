fe9c921937a8530d56e7cd5bfe224a17ca6a0ca7
Mark Ryan <mark.ryan@hmcts.net>
2019-03-28 09:57:28 +0000

    allow for username and password from secret mount

--- scripts/add-toggle.sh
+++ scripts/add-toggle.sh
@@ -9,8 +9,12 @@ export JSON_DIR="/scripts/"
 export TOGGLE_NAME="${1}"
 export DESCRIPTION="${2}"
 export TOGGLE_ENABLED="${3}"
-export ADMIN_USER="${4}"
-export ADMIN_PW="${5}"
+
+if [ "_${IMPORTER_CREDS_MOUNT}" != "_" ]; then
+  echo "Getting credentials from vault"
+  ADMIN_USERNAME=$(cat ${IMPORTER_CREDS_MOUNT}/admin-username-cmc)
+  ADMIN_PASSWORD=$(cat ${IMPORTER_CREDS_MOUNT}/admin-password-cmc)
+fi
 
 echo -e "VERBOSE=${VERBOSE} ::: JSON_DIR=${JSON_DIR}"
 echo -e "${FEATURE_TOGGLE_API_URL} ::: setting ${TOGGLE_NAME} to ${TOGGLE_ENABLED}"
@@ -19,6 +23,6 @@ envsubst < "${JSON_DIR}"toggle.template.json > "${JSON_DIR}""${TOGGLE_NAME}".jso
 
 curl ${CURL_OPTS} -X PUT \
   -H 'Content-Type: application/json' \
-  -u "${ADMIN_USER}":"${ADMIN_PW}" \
+  -u "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}" \
   -d @"${JSON_DIR}""${TOGGLE_NAME}".json \
   "${FEATURE_TOGGLE_API_URL}""${TOGGLE_NAME}"
--- scripts/add-weighted-toggle.sh
+++ scripts/add-weighted-toggle.sh
@@ -9,8 +9,12 @@ export JSON_DIR="/scripts/"
 export WEIGHTED_TOGGLE_NAME="${1}"
 export DESCRIPTION="${2}"
 export WEIGHT="${3}"
-export ADMIN_USER="${4}"
-export ADMIN_PW="${5}"
+
+if [ "_${IMPORTER_CREDS_MOUNT}" != "_" ]; then
+  echo "Getting credentials from vault"
+  ADMIN_USERNAME=$(cat ${IMPORTER_CREDS_MOUNT}/admin-username-cmc)
+  ADMIN_PASSWORD=$(cat ${IMPORTER_CREDS_MOUNT}/admin-password-cmc)
+fi
 
 echo -e "VERBOSE=${VERBOSE} ::: JSON_DIR=${JSON_DIR}"
 echo -e "${FEATURE_TOGGLE_API_URL} ::: adding ${WEIGHTED_TOGGLE_NAME} with weight ${WEIGHT} and ${DESCRIPTION}"
@@ -19,6 +23,6 @@ envsubst < "${JSON_DIR}"weighted_toggle.template.json > "${JSON_DIR}""${WEIGHTED
 
 curl ${CURL_OPTS} -X PUT \
   -H 'Content-Type: application/json' \
-  -u "${ADMIN_USER}":"${ADMIN_PW}" \
+  -u "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}" \
   -d @"${JSON_DIR}""${WEIGHTED_TOGGLE_NAME}".json \
   "${FEATURE_TOGGLE_API_URL}""${WEIGHTED_TOGGLE_NAME}"
