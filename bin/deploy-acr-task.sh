aec61fceed2c4c02c4f9fef04aba8ab71f18bec3
Tim Jacomb <tim.jacomb@hmcts.net>
2019-08-09 15:23:23 +0100

    Update deploy-acr-task.sh

--- bin/deploy-acr-task.sh
+++ bin/deploy-acr-task.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-az account set --subscription DCD-CNP-DEV
+GIT_PAT=$(az keyvault secret show --vault-name infra-vault-prod --name hmcts-github-apikey --query value -o tsv)
+
 az acr task create \
     --registry hmctspublic \
     --name task-feature-toggle-importer \
     --file acr-build-task.yaml \
     --context https://github.com/hmcts/feature-toggle-importer.git \
     --branch master \
-    --git-access-token $GITHUB_TOKEN
+    --git-access-token $GIT_PAT \
+    --subscription DCD-CNP-PROD
